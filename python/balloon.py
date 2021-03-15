050f763163f5b61f7e7d354e25922ab0467662f7
Banu <banu@Sergeys-MacBook-Pro.local>
2021-03-14 18:34:32 -0700

    optimized

--- python/balloon.py
+++ python/balloon.py
@@ -3,8 +3,8 @@ from typing import List
 
 def maxNumberOfBalloons(text: str) -> int:
         letters = {}
-        balloon = ['b', 'a', 'l', 'l', 'o', 'o', 'n']
-        count, precount = 0, 0
+        balloon = {'b': 1, 'a': 1, 'l': 1, 'o': 1, 'n': 1}
+        count = 0
         
         for c in text:
             if c in balloon:
@@ -12,21 +12,20 @@ def maxNumberOfBalloons(text: str) -> int:
                     letters[c] += 1
                 else:
                     letters[c] = 1
-        if len(letters) < 5:
-            return 0
+        if (len(letters) < 5 or letters['o'] < 2 or letters['l'] < 2):
+            return count
+
+        letters['o'] //= 2
+        letters['l'] //= 2
+
+        count = min(letters.values())
         
-        while letters:
-            precount = 0
-            for i in balloon:
-                if i in letters.keys():
-                    precount += 1
-                    letters[i] -= 1
-                    if letters[i] == 0:
-                        del letters[i]
-                if precount == 7:
-                    count += 1
-       
         return count
+        
+        
+        
+       
+        
 if __name__ == '__main__':
     print(maxNumberOfBalloons('"loonbalxballpoon"'))
     
