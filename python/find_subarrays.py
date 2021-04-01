859b518426208698cf96dbafc9c4dd1f24a9be39
Banu <banu@Sergeys-MacBook-Pro.local>
2021-04-01 10:11:07 -0700

    subbarrays with product less than target

---
+++ python/find_subarrays.py
@@ -0,0 +1,29 @@
+# Given an array with positive numbers and a target number
+# find all of its contiguous subarrays
+# whose product is less than the target number
+from collections import deque
+
+
+def find_subarrays(arr, target):
+    result = []
+    product = 1
+    left = 0
+    for right in range(len(arr)):
+        product *= arr[right]
+        
+        while (product >= target and left < len(arr)):
+            product /= arr[left]
+            left += 1
+        
+        temp_list = deque()
+        
+        for i in range(right, left-1, -1):
+            temp_list.appendleft(arr[i])
+            result.append(list(temp_list))
+    
+    return result
+
+if __name__ == '__main__':
+    print(find_subarrays([2, 5, 3, 10], 30))
+    print(find_subarrays([8, 2, 6, 5, 3], 50))
+        
